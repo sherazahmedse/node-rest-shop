e3aaa06624393dc12b81bb0a491e23e05fecada7
sherazahmedse <sherazahmed.cs@gmail.com>
2020-02-28 18:43:48 +0500

    user model updated

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 
 const User = require('../models/user.js');
 
 router.post('/signup' , (req,res,next) => {
     console.log('User Sign up'); 
 
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: req.body.password
+    bcrypt.hash( req.body.password,10,(err ,hash)=> {
+        if(err){
+            return res.status(500).json({
+                error: err
+            });
+        }
+        else{
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+            });
+        
+        }
     });
+
 
 });
 
